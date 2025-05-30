26d041abe328033c29d3833920bf111791bafee4
Alexis Giles <alexisgiles00@gmail.com>
2025-05-30 14:15:45 -0600

    Corrección de home referencia registro

--- views/home.php
+++ views/home.php
@@ -1,3 +1,3 @@
 <h1>Bienvenido a PrimerPaso</h1>
 <p>Conectamos a estudiantes y recién egresados con oportunidades reales de prácticas, becas y empleos.</p>
-<a href="?page=register-user">Registrarse</a> | <a href="?page=login">Iniciar sesión</a>
+<a href="?page=registro-usuario">Registrarse</a> | <a href="?page=login">Iniciar sesión</a>
